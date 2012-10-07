f1339ab95acb1fd2f9df93da127d4853a9532133
iasincru <>
2012-10-07 12:09:53 +0000

    Added some 'try - exception' for correct file handling. Rearrange some if statements to improve DSs handling

--- Bookkeeping.py
+++ Bookkeeping.py
@@ -3,7 +3,7 @@ from DatasetInfo import *
 
 class Bookkeeping:
 
-    filename="harvesting_bookkeeping.txt"
+    filename="harvesting_bookkeeping.txt" #Attention: currently different file is used for bookkeeping
     json={}
 
     def __init__(self, *args, **kwargs):
@@ -15,8 +15,12 @@ class Bookkeeping:
         
         if "file" in kwargs:
             self.filename=kwargs["file"]
-        
-        file=open(self.filename,"r")
+        try:
+            file=open(self.filename,"r")
+        except IOError, error:
+            print "Error caught while opening file '"+self.filename+"'"
+            print error.__str__()
+            return 0
         JSON=file.readlines()[0].split('\n')[0]
         file.close()
         print JSON
@@ -24,6 +28,8 @@ class Bookkeeping:
                
     def compare(self, DSs):
         for ds in DSs:
+            if (len(ds.runs)==0):
+                DSs.remove(ds)
             if ds.name in self.json:
                 for run in ds.runs:
                     if run in self.json[ds.name]:
@@ -37,8 +43,6 @@ class Bookkeeping:
                     if (ds.castor_check(run)):
                         ds.runs.remove( run)
                         self.append( ds.name, run)
-            if (len(ds.runs)==0):
-                DSs.remove(ds)
 
     def append(self, ds, runs):
         if not (ds in self.json):
@@ -54,6 +58,11 @@ class Bookkeeping:
 
     def dump(self):
         print self.json
-        file=open(self.filename,"w")
+        try:
+            file=open(self.filename,"w")
+        except IOError, error:
+            print "Error caught while opening file '"+self.filename+"'"
+            print error.__str__()
+            return 0
         file.write( ( json.dumps( self.json)))
         file.close()
