344085428d9c670128a27f8bc715b658db3647ba
iasincru <>
2013-03-13 10:49:32 +0000

    Update to avoid missing enviroment variables

--- preHarvester.sh
+++ preHarvester.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+. /afs/cern.ch/cms/cmsset_default.sh ;
+
 ## This small script is needed to be able to run the harvester.
 ## 1st: CRAB to be able to submit crab jobs
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh ;
@@ -8,9 +10,10 @@ source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh ;
 ## unfortunatelly one CMSSW version must be selected, and must be installed in the indicated path
 ## To be able to run the harvester, please be aware that ONLY the 
 cwd=$PWD ;
-cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting ;
+basedir="/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/harvesting" ;
+cd $basedir;
 scram project CMSSW CMSSW_5_3_8 ;
-cd CMSSW_5_3_8/src ;
+cd $basedir/CMSSW_5_3_8/src ;
 eval `scramv1 runtime -sh` ;
 cd $cwd ;
 export X509_USER_PROXY=$HOME/x509up ;
