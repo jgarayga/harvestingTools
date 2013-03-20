e0825c9c791816a0a8c863fa51d87d1be4e9fd22
iasincru <>
2013-03-20 11:10:36 +0000

    Fix bug

--- preHarvester.sh
+++ preHarvester.sh
@@ -7,5 +7,5 @@
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh ;
 
 ## 2nd: export DBS libraries
-export PYTHONBASE=/afs/cern.ch/cms/slc5_amd64_gcc462/cms/dbs-client/DBS_2_1_1_patch1_1/lib/ ;
+export PYTHONPATH=/afs/cern.ch/cms/slc5_amd64_gcc462/cms/dbs-client/DBS_2_1_1_patch1_1/lib/ ;
 export X509_USER_PROXY=$HOME/x509up ;
